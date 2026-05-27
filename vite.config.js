a36b3fa51fa391c20849e99e78d336bd1ade20fb
Christoph Herrmann <github@sharaal.de>
2026-05-27 09:04:19 +0200

    feat: Deployed version should use absolute URLs to the assets

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,20 @@ import { readFile, writeFile } from "node:fs/promises";
 import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 
+function assetUrl(fileName) {
+  const publicBaseUrl = process.env.PUBLIC_BASE_URL?.replace(/\/+$/, "");
+
+  if (publicBaseUrl) {
+    return `${publicBaseUrl}/${fileName}`;
+  }
+
+  return `./${fileName}`;
+}
+
 function localIndexHtml() {
+  const cssUrl = assetUrl("dragonwisdom.css");
+  const jsUrl = assetUrl("dragonwisdom.js");
+
   return {
     name: "local-index-html",
     transformIndexHtml: {
@@ -12,11 +25,11 @@ function localIndexHtml() {
         return html
           .replace(
             '<script type="module" crossorigin src="./dragonwisdom.js"></script>',
-            '<script src="./dragonwisdom.js"></script>'
+            `<script src="${jsUrl}"></script>`
           )
           .replace(
             '<link rel="stylesheet" crossorigin href="./dragonwisdom.css">',
-            '<link rel="stylesheet" href="./dragonwisdom.css">'
+            `<link rel="stylesheet" href="${cssUrl}">`
           );
       }
     },
@@ -25,7 +38,7 @@ function localIndexHtml() {
       const cssIndexPath = resolve("dist/public/index-css.html");
       const index = await readFile(indexPath, "utf8");
       const cssIndex = index
-        .replace('    <script src="./dragonwisdom.js"></script>\n', "")
+        .replace(`    <script src="${jsUrl}"></script>\n`, "")
         .replace('<a href="./index-css.html">CSS Version</a>', '<a href="./index.html">JavaScript Version</a>');
 
       await writeFile(cssIndexPath, cssIndex);
